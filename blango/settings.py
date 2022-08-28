42b722d1a6c3d7787ba1c4e03abb03704ecba423
Art Beckman <aabeckman@yahoo.com>
2022-08-28 23:01:20 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -142,6 +142,47 @@ class Dev(Configuration):
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "filters": {
+            "require_debug_false": {
+                "()": "django.utils.log.RequireDebugFalse",
+            },
+        },
+        "formatters": {
+            "verbose": {
+                "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+                "style": "{",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "verbose",
+            },
+            "mail_admins": {
+                "level": "ERROR",
+                "class": "django.utils.log.AdminEmailHandler",
+                "filters": ["require_debug_false"],
+            },
+        },
+        "loggers": {
+            "django.request": {
+                "handlers": ["mail_admins"],
+                "level": "ERROR",
+                "propagate": True,
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    }
+
+    ADMINS = [("Art Beckman", "aabeckman@yahoo.com")]
+
 class Prod(Dev):
     DEBUG = False
     SECRET_KEY = values.SecretValue()
--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,16 @@ from django.utils import timezone
 from blog.models import Post
 from django.shortcuts import render, get_object_or_404, redirect
 from blog.forms import CommentForm
+import logging
 
+logger = logging.getLogger(__name__)
 def index(request):
   posts = Post.objects.all #.filter(published_at__lte=timezone.now())
   # previous line having issues when published_at is null
+  logger.debug("Got %d posts", len(posts()))
   return render(request, "blog/index.html", {"posts": posts})
 
+logger = logging.getLogger(__name__)
 def post_detail(request, slug):
   post = get_object_or_404(Post, slug=slug)
   if request.user.is_active:
@@ -18,6 +22,9 @@ def post_detail(request, slug):
         comment = comment_form.save(commit=False)
         comment.content_object = post
         comment.creator = request.user
+        logger.info(
+          "Created comment on Post %d for user %s", post.pk, request.user
+        )
         comment.save()
         return redirect(request.path_info)
     else:
